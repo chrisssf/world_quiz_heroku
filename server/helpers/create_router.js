d59dde1c8a86454db01269390d8c847a87131248
Chris Fraser <chrisssfraser@gmail.com>
2020-09-07 02:50:51 +0100

    getting rady for heroku

--- server/helpers/create_router.js
+++ server/helpers/create_router.js
@@ -17,8 +17,6 @@ const createRouter = function(collection) {
       });
   });
 
-
-
   return router
 };
 
--- server/server.js
+++ server/server.js
@@ -10,19 +10,60 @@ app.use(bodyParser.json());
 const MongoClient = require('mongodb').MongoClient;
 const createRouter = require('./helpers/create_router.js');
 
-MongoClient.connect('mongodb://localhost:27017')
+
+// original code for connecting to local DB!!!!!!!
+// MongoClient.connect('mongodb://localhost:27017')
+//   .then((client) => {
+//     const db = client.db('quiz_questions');
+//     const countriesCollection = db.collection('countries');
+//     const capitalsCollection = db.collection('capitals');
+//     const flagsCollection = db.collection('flags'); 
+
+//   app.use( '/api/countries', createRouter( countriesCollection ));
+//   app.use( '/api/capitals', createRouter( capitalsCollection ));
+//   app.use( '/api/flags', createRouter( flagsCollection ));
+// })
+// .catch( console.error );
+
+// new code!!!!!!!!!!
+MongoClient.connect('mongodb+srv://Chrisssf:Chrimssf@freecluster.zfuvg.mongodb.net/world_quiz?retryWrites=true&w=majority')
   .then((client) => {
-    const db = client.db('quiz_questions');
+    const db = client.db('world_quiz');
     const countriesCollection = db.collection('countries');
     const capitalsCollection = db.collection('capitals');
-    const flagsCollection = db.collection('flags'); //!!!!!!!!!!
+    const flagsCollection = db.collection('flags'); 
 
   app.use( '/api/countries', createRouter( countriesCollection ));
   app.use( '/api/capitals', createRouter( capitalsCollection ));
-  app.use( '/api/flags', createRouter( flagsCollection ));//!!!!!!!!!!
+  app.use( '/api/flags', createRouter( flagsCollection ));
 })
 .catch( console.error );
 
-app.listen( 3000, function() {
-  console.log( `World Quiz server running on port ${this.address().port}` );
-});
+// may need this line to remove warning, see 15:33 in video 1.....
+// useNewUrlParser: true 
+
+
+
+// original code for running locally!!!!!!!!!!!!
+// app.listen( 3000, function() {
+//   console.log( `World Quiz server running on port ${this.address().port}` );
+// });  
+
+
+// Handle Production
+
+if (process.env.NODE_ENV === "production") {
+  // Static folder
+  app.use(express.static(__dirname + '/public'))
+
+  // handle Single Page Aplication
+  app.get(/.*/)
+}
+
+
+// New code for heroku. Uses port assigned by heroku OR if not on heroku, use originall port!!!!!!
+const port = process.env.PORT || 3000
+
+app.listen(port, function(){
+  console.log(`Server started running on port ${port}`)
+})
